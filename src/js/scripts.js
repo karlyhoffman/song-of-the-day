aa6152929804d3a91d1a1b757e90c234197b6c9a
Karly <karlyhoffman@gmail.com>
2018-02-02 06:46:17 -0600

    date change tweak

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,7 +8,7 @@
     $( ".post-cont" ).scroll(function() {
       $('.post').each(function() {
         var postTop = $(this).offset().top;
-        var postBottom = $(this).offset().top + ($(this).height());
+        var postBottom = $(this).offset().top + ($(this).height() * 1.5 );
         var postDate = $(this).attr("data-date");
 
         if ( $( ".post-cont" ).scrollTop() >= postTop
